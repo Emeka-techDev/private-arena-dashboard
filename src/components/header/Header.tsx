60754c69e643912cb72643ed2e562875e420ad02
Emeka-techDev <gilbertgenye@gmail.com>
2026-04-03 14:30:37 +0100

    updated arena logo

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -10,38 +10,37 @@ interface NavBarProps {
 }
 
 export const Header = ({ onMobileMenuOpen }: NavBarProps) => {
-    const { isDark }   = useTheme();
+    // const { isDark }   = useTheme();
 
-    const textSec     = isDark ? "text-[#8B91A8]"  : "text-[#1E2235]";
-	const btnBg    = isDark ? "bg-bgColor3 border-white/[0.08]" : "bg-[#E8EAF0] border-[#C5C9D6]/60";
+    // const textSec     = isDark ? "text-[#8B91A8]"  : "text-[#1E2235]";
+	// const btnBg    = isDark ? "bg-bgColor3 border-white/[0.08]" : "bg-[#E8EAF0] border-[#C5C9D6]/60";
   
 	return (
 		<nav className='flex justify-between items-center z-40 min-w-full  bg-primary-white px-[60px] py-[21px] sticky top-0'>
-		<img src={blueLogo} alt='arena logo' className='hidden md:block h-8' />
+			<img src={blueLogo} alt='arena logo' className='hidden md:block h-8' />
 
-		<button
-			className={`lg:hidden flex items-center justify-center w-8 h-8 rounded-lg border transition-all ${btnBg} ${textSec} hover:border-customTealLight hover:text-customTealLight`}
-			onClick={onMobileMenuOpen}
-			>
+			<button
+				className={`lg:hidden flex items-center justify-center w-20 h-10 rounded-lg `}
+				onClick={onMobileMenuOpen}
+				>
 
-				 
-                            <img src={blueLogo} alt='arena logo' className='h-8 cursor-pointer' />
-		</button>
-		
-		<div className='gap-4 flex items-center '>
-			<div className='h-10 w-10 grid place-content-center bg-primary-blue20  rounded-full cursor-pointer'>
-			<Bell />
+				<img src={blueLogo} alt='arena logo' className='h-10 cursor-pointer' />
+			</button>
+			
+			<div className='gap-4 flex items-center '>
+				<div className='h-10 w-10 grid place-content-center bg-primary-blue20  rounded-full cursor-pointer'>
+				<Bell />
+				</div>
+				<div className='flex items-center gap-[10px]'>
+				<img
+					src={AvatarImage}
+					alt='user avatar'
+					className='h-10 w-10  object-cover rounded-full'
+				/>
+
+				<CheveronDown className='cursor-pointer' />
+				</div>
 			</div>
-			<div className='flex items-center gap-[10px]'>
-			<img
-				src={AvatarImage}
-				alt='user avatar'
-				className='h-10 w-10  object-cover rounded-full'
-			/>
-
-			<CheveronDown className='cursor-pointer' />
-			</div>
-		</div>
 		</nav>
 	);
 };
